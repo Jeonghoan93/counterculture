b65965369b3f40403a4fe9db2b4e29bc2e50ddf4
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-30 21:08:09 +0200

    yes

--- react/src/pages/AnimationPage/FullPageModal.tsx
+++ react/src/pages/AnimationPage/FullPageModal.tsx
@@ -72,7 +72,7 @@ const FullPageModal: React.FC = () => {
           </div>
 
           <h2 className="text-gray-300 font-bold text-[12pt] mb-2 font-mono">
-            The most destructive plan
+            The Impossible plan
           </h2>
         </div>
         <button
